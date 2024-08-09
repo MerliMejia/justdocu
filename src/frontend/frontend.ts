8770840c54b750a5ae3a3e273d9da5bd2112715e
Merli Mejia <merli@beatsunion.com>
2024-08-09 05:38:28 -0400

    Displaying data on the FE.

--- src/frontend/frontend.ts
+++ src/frontend/frontend.ts
@@ -1,1 +1,32 @@
-console.log("THIS IS FROM THE FRONTEND");
+type DbItemType = {
+  title: string;
+  desc: string;
+};
+
+type DbDataType = {
+  items: DbItemType[];
+};
+
+const constructItem = (item: DbItemType) => {
+  const string = `<div class="item-container">
+          <h4>${item.title}</h4>
+          <p>${item.desc}</p>
+        </div>`;
+
+  return string;
+};
+
+const drawITems = (items: DbItemType[]) => {
+  const element = document.getElementById("items");
+
+  if (element) {
+    const allItemsString = items.map((item) => constructItem(item)).join("");
+    element.innerHTML = allItemsString;
+  }
+};
+
+window.onload = async (e) => {
+  const data: DbDataType = await fetch("/items").then((r) => r.json());
+  console.log("data: ", data);
+  drawITems(data.items);
+};
